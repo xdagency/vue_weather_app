d6d0dac61194196f19522ca1ba2e5e812f399e5f
xdagency <pandabear00001@outlook.com>
2020-12-13 23:21:37 -0500

    process env

--- src/App.vue
+++ src/App.vue
@@ -80,7 +80,7 @@
 
         this.loader = 'show';
 
-        axios.get(`${config.WEATHER_API_URL}/?q=${location}&appid=${config.WEATHER_API_KEY}&units=metric`)
+        axios.get(`${config.WEATHER_API_URL || process.env.WEATHER_API_URL}/?q=${location}&appid=${config.WEATHER_API_KEY || process.env.WEATHER_API_KEY}&units=metric`)
             .then(results => {
               // overwrite weather object with new data
               this.weather = results.data;
@@ -103,9 +103,9 @@
     mounted() {
       axios.all([
           // copydeck api
-          axios.get(`${config.COPYDECK_URL}`, { headers: { 'Authorization': 'Bearer ' +  config.COPYDECK_API_KEY }}),
+          axios.get(`${config.COPYDECK_URL || process.env.COPYDECK_URL}`, { headers: { 'Authorization': 'Bearer ' +  config.COPYDECK_API_KEY || process.env.COPYDECK_API_KEY }}),
           // weather api
-          axios.get(`${config.WEATHER_API_URL}/?q=${this.location}&appid=${config.WEATHER_API_KEY}&units=metric`)
+          axios.get(`${config.WEATHER_API_URL || process.env.WEATHER_API_URL}/?q=${this.location}&appid=${config.WEATHER_API_KEY || process.env.WEATHER_API_KEY}&units=metric`)
         ])
         .then(results => {
 
