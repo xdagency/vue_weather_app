c0430b096cc6cf51a009a804e7d1526acb77401a
xdagency <pandabear00001@outlook.com>
2021-01-04 11:57:42 -0500

    fixed some styling issues with the search bar

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -6,7 +6,7 @@
         <img :src="'https://www.countryflags.io/' + country_code + '/flat/32.png'" :alt="'Country flag for ' + country_code" title="Flag icon">
       </figure>
 
-      <input type="text" name="search" v-model.lazy="searchLocation" class="search" :placeholder="copy_en.PLACEHOLDER_ENTER_CITY + ' ' + copy_jp.PLACEHOLDER_ENTER_CITY">
+      <input type="search" name="search" v-model.lazy="searchLocation" class="search" id="search" :placeholder="copy_en.PLACEHOLDER_ENTER_CITY + ' ' + copy_jp.PLACEHOLDER_ENTER_CITY">
 
     </form>
 
@@ -39,6 +39,7 @@ form {
 }
 
 input.search {
+  box-sizing: border-box;
   width: 100%;
   padding: 4px 24px;
   font-size: 56px;
@@ -47,27 +48,39 @@ input.search {
   font: 'Noto Sans SC', sans-serif;
   font-weight: 300;
   // border-radius: 10px;
+  &:hover {
+    background: rgba(255,255,255,0.1);
+  }
   &:focus {
     background: rgba(255,255,255,0.33);
     outline: none;
   }
 
+  &::-webkit-search-cancel-button {
+    // position:relative;
+    // -webkit-appearance: none;
+    // background: red;
+    // height: 20px;
+    // width: 20px;
+    // border-radius:10px;
+  }
+
   @media screen and (min-width: 720px) {
-    padding: 4px calc(96px + 48px);
+    padding: 4px 96px;
     font-size: 72px;
   }
   @media screen and (min-width: 1280px) {
-    padding: 4px calc(196px + 48px);
+    padding: 4px 196px;
   }
   @media screen and (min-width: 1600px) {
-    padding: 4px calc(296px + 48px);
+    padding: 4px 296px;
   }
 }
 
 figure.flag {
   display: none;
   position: absolute;
-  left: 96px;
+  left: 48px;
   bottom: 26px;
   padding: 0;
   margin: 0;
@@ -82,10 +95,10 @@ figure.flag {
     display: block;
   }
   @media screen and (min-width: 1280px) {
-    left: 196px;
+    left: 148px;
   }
   @media screen and (min-width: 1600px) {
-    left: 296px;
+    left: 248px;
   }
 
 }
